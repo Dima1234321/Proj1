dc117dfbb7906644a793d53124f4b4eef2378b64
D R <dima1234321@gmail.com>
2020-03-24 23:04:36 +0200

    change existing code

--- src/Pkg1/Main.java
+++ src/Pkg1/Main.java
@@ -2,7 +2,7 @@ package Pkg1;
 
 public class Main {
     public static void main(String[] args) {
-        Point x = new Point(0,0);
+        Point x = new Point(1,1);
         double dist = x.calcDistance(new Point(10,20));
         System.out.println(dist);
     }
