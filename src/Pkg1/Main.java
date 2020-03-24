727acf105c2d4d1771e5926e44890e565c358d33
D R <dima1234321@gmail.com>
2020-03-24 23:51:23 +0200

    change main - 3, 3

--- src/Pkg1/Main.java
+++ src/Pkg1/Main.java
@@ -2,7 +2,7 @@ package Pkg1;
 
 public class Main {
     public static void main(String[] args) {
-        Point x = new Point(20,20);
+        Point x = new Point(3,3);
         double dist = x.calcDistance(new Point(10,20));
         System.out.println(dist);
     }
