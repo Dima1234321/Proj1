f91141587961bb7b63f542a1c5a69a6487cde183
D R <dima1234321@gmail.com>
2020-03-25 01:40:35 +0200

    50 50

--- src/Pkg1/Main.java
+++ src/Pkg1/Main.java
@@ -2,7 +2,7 @@ package Pkg1;
 
 public class Main {
     public static void main(String[] args) {
-        Point x = new Point(5,5);
+        Point x = new Point(50,50);
         double dist = x.calcDistance(new Point(10,20));
         System.out.println(dist);
     }
