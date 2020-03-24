43162b6d0f0b289ad1fc8c2a0709182f3a5ac472
D R <dima1234321@gmail.com>
2020-03-24 23:37:15 +0200

    change existing code after create branch1

--- src/Pkg1/Main.java
+++ src/Pkg1/Main.java
@@ -2,7 +2,7 @@ package Pkg1;
 
 public class Main {
     public static void main(String[] args) {
-        Point x = new Point(2,2);
+        Point x = new Point(20,20);
         double dist = x.calcDistance(new Point(10,20));
         System.out.println(dist);
     }
